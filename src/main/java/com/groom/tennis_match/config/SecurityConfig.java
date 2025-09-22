f03e5d73ac849c5e76d6b8b0d0d965a2f9617221
kmgyu <lidersy961@gmail.com>
2025-09-22 17:09:37 +0900

    feat: 권한 부족 시 401 unauthorized 반환 핸들러 추가

--- src/main/java/com/groom/tennis_match/config/SecurityConfig.java
+++ src/main/java/com/groom/tennis_match/config/SecurityConfig.java
@@ -2,6 +2,7 @@
 package com.groom.tennis_match.config;
 
 import com.groom.tennis_match.auth.filter.JsonUsernamePasswordAuthFilter;
+import com.groom.tennis_match.auth.handler.AuthAccessDeniedHandler;
 import com.groom.tennis_match.auth.handler.AuthFailureHandler;
 import com.groom.tennis_match.auth.handler.AuthLogoutSuccessHandler;
 import com.groom.tennis_match.auth.handler.AuthSuccessHandler;
@@ -19,6 +20,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -61,6 +63,9 @@ public class SecurityConfig {
         return new AuthLogoutSuccessHandler();
     }
 
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() { return new AuthAccessDeniedHandler(); }
+
     // Custom JSON login filter as a bean (AuthenticationManager 주입)
     @Bean
     public JsonUsernamePasswordAuthFilter jsonUsernamePasswordAuthFilter(
@@ -90,7 +95,7 @@ public class SecurityConfig {
 
         http
                 .securityContext(sc -> sc.requireExplicitSave(false))   // 세션 발급
-                // 세션 기반: 필요 시 생성
+                // 세션 기반
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
 
                 // CSRF: 기본 활성화, 특정 경로만 예외 (H2, JSON 로그인)
@@ -100,6 +105,11 @@ public class SecurityConfig {
 //                                new AntPathRequestMatcher("/h2-console/**"),
 //                                new AntPathRequestMatcher("/api/admin/**")
 //                        )
+                )
+
+                // 접근 권한 부족 시 해당 핸들러 이용
+                .exceptionHandling(ex -> ex
+                        .accessDeniedHandler(accessDeniedHandler()) // unauthorized 반환
                 )
 
                 // H2 콘솔 프레임 허용(로컬 개발용)
