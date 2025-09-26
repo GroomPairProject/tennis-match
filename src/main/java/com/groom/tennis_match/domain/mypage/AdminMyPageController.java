fa5588200f25a6cc51d72473ef13012d755d8d61
kmgyu <lidersy961@gmail.com>
2025-09-26 16:30:23 +0900

    refactor: 쿠키 정보 조회 방식 수정 - HttpSession이 아닌 @AuthenticationPrincipal로 조회

--- src/main/java/com/groom/tennis_match/domain/mypage/AdminMyPageController.java
+++ src/main/java/com/groom/tennis_match/domain/mypage/AdminMyPageController.java
@@ -1,17 +1,15 @@
 package com.groom.tennis_match.domain.mypage;
 
+import com.groom.tennis_match.auth.entity.Admin;
 import com.groom.tennis_match.common.constant.SuccessCode;
 import com.groom.tennis_match.common.dto.ApiResponse;
 import com.groom.tennis_match.domain.mypage.dto.AdminProfileDTO;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Enumeration;
 
 @Slf4j
 @RequestMapping("/api")
@@ -21,19 +19,8 @@ public class AdminMyPageController {
   private final AdminMyPageService adminMyPageService;
 
   @GetMapping("/admin/mypage")
-  public ApiResponse<AdminProfileDTO> getAdminProfile(HttpServletRequest request) {
-
-    HttpSession session = request.getSession(false);
-    String username = (String) session.getAttribute("username");
-    log.info("log of username = {}", username);
-
-    Enumeration<String> names = session.getAttributeNames();
-    while (names.hasMoreElements()) {
-      String name = names.nextElement();
-      Object value = session.getAttribute(name);
-      log.info("session attribute: {} = {}", name, value);
-    }
-    return ApiResponse.success(adminMyPageService.getAdminProfile(username), SuccessCode.USER_UPDATE_SUCCESS);
+  public ApiResponse<AdminProfileDTO> getAdminProfile(@AuthenticationPrincipal Admin admin) {
+    return ApiResponse.success(adminMyPageService.getAdminProfile(admin.getUsername()), SuccessCode.USER_UPDATE_SUCCESS);
 
   }
 }
