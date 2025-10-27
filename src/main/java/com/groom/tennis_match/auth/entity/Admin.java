99640ad60430e8797f9603a2a489d6205dd1eff8
kmgyu <lidersy961@gmail.com>
2025-10-27 22:08:51 +0900

    valid: user details 상태 메서드 수정

--- src/main/java/com/groom/tennis_match/auth/entity/Admin.java
+++ src/main/java/com/groom/tennis_match/auth/entity/Admin.java
@@ -100,6 +100,7 @@ public class Admin extends BaseTimeEntity implements UserDetails {
         return this.username;
     }
 
+    // 계정 유효 기간 만료
     @Override
     public boolean isAccountNonExpired() {
         return UserDetails.super.isAccountNonExpired();
@@ -107,9 +108,10 @@ public class Admin extends BaseTimeEntity implements UserDetails {
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return this.isLock();
     }
 
+    // 비밀번호 만료
     @Override
     public boolean isCredentialsNonExpired() {
         return UserDetails.super.isCredentialsNonExpired();
@@ -117,6 +119,6 @@ public class Admin extends BaseTimeEntity implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return this.isActive();
     }
 }
